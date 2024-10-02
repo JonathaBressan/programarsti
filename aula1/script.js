e117b2a43b7ea01f73f6ba4b3f4395d98c3800de
Jonatha Geske Bressan <132519815+JonathaBressan@users.noreply.github.com>
2024-10-02 20:31:58 -0300

    rr

--- aula1/script.js
+++ aula1/script.js
@@ -2,6 +2,16 @@
 $(document).ready(function(){
     
     $("input[name=cep]").mask("00000-000");
+    $("input[name=numero]").mask("n", {
+        translation: {
+            'n': {
+                pattern: /[0-9]/,
+                recursive: true
+            }
+        }
+    });
+
+
     $("form").on("submit", function(event) {
         //interrompe o evento.
         event.stopPropagation();

25056782f8bff9357aa06a06f764226de5a9bff8
Jonatha Geske Bressan <132519815+JonathaBressan@users.noreply.github.com>
2024-10-02 22:13:29 -0300

    yyy

--- aula1/script.js
+++ aula1/script.js
@@ -31,17 +31,57 @@ $(document).ready(function(){
             $.ajax("https://viacep.com.br/ws/" + cep + "/json")
             .done(function (data){
                 let resposta = JSON.parse(data);
-                if (!resposta.erro) {
+                if (resposta.erro) {
+                    
+                    if(!cep.length == 0){
+                        $("input[name=cep]").addClass("is-invalid");
+                    }
+                } else {
                     $("input[name=rua]").val(resposta.logradouro);
                     $("input[name=complemento]").val(resposta.complemento);
                     $("input[name=bairro]").val(resposta.bairro);
-                    $("select[name=estado]").val(resposta.uf);
-                    $("input[name=localidade]").val(resposta.localidade);
-                    return; //em caso de erro sai da funcao
+                    $("select[name=estado]").val(resposta.uf).change();
+                    $("select[name=localidade]").val(resposta.localidade).change();
                 }
-                alert(data);
             });
             
+        }
+    });
+    const urlEstados = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados';    
+    // Carregar os estados na inicialização
+    $.getJSON(urlEstados, function(data) {
+        $('#estado').empty();
+        $('#estado').append(`<option value="">Selecione o estado</option>`); // Adiciona a opção padrão
+        data.sort(function(a, b) {
+            return a.nome.localeCompare(b.nome);
+        });
+        
+        data.forEach(function(estado) {
+            $('#estado').append(`<option value="${estado.sigla}">${estado.nome}</option>`);
+        });
+    });
+
+    $('#estado').on('change', function() {
+        let estadoId = $(this).val(); // Pega o ID do estado selecionado
+
+        if (estadoId) {
+            const urlCidades = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${estadoId}/municipios`;
+
+            $.getJSON(urlCidades, function(data) {
+                $('#cidade').empty(); // Limpa o select de cidades
+                $('#cidade').append(`<option value="">Selecione a cidade</option>`); // Adiciona a opção padrão
+
+                data.sort(function(a, b) {
+                    return a.nome.localeCompare(b.nome);
+                });
+
+                data.forEach(function(cidade) {
+                    $('#cidade').append(`<option value="${cidade.nome}">${cidade.nome}</option>`);
+                });
+            });
+        } else {
+            $('#cidade').empty(); // Limpa o select de cidades caso não haja estado selecionado
+            $('#cidade').append(`<option value="">Primeiro selecione o estado</option>`);
         }
     });
 });
